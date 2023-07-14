ce428ae7695e9e862860d4a35f9badf1a06dec20
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2023-07-14 03:08:32 +0300

    feat: implement Header component

---
+++ async-race/src/ts/components/header/Header.ts
@@ -0,0 +1,81 @@
+import state from '../../data/state';
+import { Pages } from '../../enums';
+import EventEmitter from '../../utils/EventEmitter';
+import Component from '../Component';
+import { NAVIGATION_ITEMS_CONTENT, PAGES_LIST } from './constants';
+
+export default class Header extends Component {
+  private eventEmitter: EventEmitter;
+
+  constructor(eventEmitter: EventEmitter) {
+    super({
+      tagName: 'header',
+      classNames: ['header'],
+    });
+
+    this.eventEmitter = eventEmitter;
+
+    this.render();
+  }
+
+  private render(): void {
+    const containerComponent = new Component({ classNames: ['header__container', 'container'] });
+    const bodyComponent = new Component({ classNames: ['header__body'] });
+    const titleComponent = new Component({ tagName: 'h1', classNames: ['header__title'], textContent: 'ASYNC RACE' });
+    const navigationMenuComponent = this.getRenderedNavigationMenuComponent();
+
+    this.append(containerComponent);
+    containerComponent.append(bodyComponent);
+    bodyComponent.append(titleComponent, navigationMenuComponent);
+  }
+
+  private getRenderedNavigationMenuComponent(): Component {
+    const navigationMenuComponent = new Component({ tagName: 'nav', classNames: ['header__nav', 'header-nav-menu'] });
+    const navigationMenuListComponent = this.getRenderedNavigationMenuListComponent();
+
+    navigationMenuComponent.append(navigationMenuListComponent);
+
+    return navigationMenuComponent;
+  }
+
+  private getRenderedNavigationMenuListComponent(): Component {
+    const navigationMenuListComponent = new Component({ tagName: 'ul', classNames: ['header-nav-menu__list'] });
+
+    for (let i = 0; i < NAVIGATION_ITEMS_CONTENT.length; i += 1) {
+      const navigationItemComponent = new Component({ tagName: 'li', classNames: ['header-nav-menu__item'] });
+      const buttonComponent = new Component({
+        tagName: 'button',
+        classNames: ['header-nav-menu__btn', 'btn'],
+        textContent: NAVIGATION_ITEMS_CONTENT[i],
+        attributes: { type: 'button', 'data-page': PAGES_LIST[i] },
+      });
+
+      navigationItemComponent.append(buttonComponent);
+      navigationMenuListComponent.append(navigationItemComponent);
+
+      this.handleNavigationButtonListener(buttonComponent);
+    }
+
+    return navigationMenuListComponent;
+  }
+
+  private handleNavigationButtonListener(buttonComponent: Component): void {
+    buttonComponent.addListener('click', async () => {
+      const pageData: string | undefined = buttonComponent.getNode().dataset.page;
+
+      if (state.currentPage !== pageData) {
+        if (pageData === Pages.garage) {
+          state.currentPage = Pages.garage;
+        } else if (pageData === Pages.leaderboard) {
+          state.currentPage = Pages.leaderboard;
+        }
+
+        this.eventEmitter.emit('leaderboard-pagination: set visibility');
+        this.eventEmitter.emit('leaderboard: set visibility');
+        this.eventEmitter.emit('control-menu: set visibility');
+        this.eventEmitter.emit('garage-pagination: set visibility');
+        this.eventEmitter.emit('garage: set visibility');
+      }
+    });
+  }
+}
---
+++ async-race/src/ts/components/header/constants.ts
@@ -0,0 +1,2 @@
+export const NAVIGATION_ITEMS_CONTENT: string[] = ['TO GARAGE', 'TO WINNERS'];
+export const PAGES_LIST: string[] = ['garage', 'leaderboard'];
